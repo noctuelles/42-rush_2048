98c75a44fafd97b07502d7d18a5a582b56d7f131
Guillaume SALLE <gusalle@student.42.fr>
2022-03-19 19:20:58 +0100

    move_merge

--- move_merge.c
+++ move_merge.c
@@ -6,117 +6,172 @@
 /*   By: gusalle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 15:34:42 by gusalle           #+#    #+#             */
-/*   Updated: 2022/03/19 16:25:36 by gusalle          ###   ########.fr       */
+/*   Updated: 2022/03/19 19:05:52 by gusalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "2048.h"
 
-static	move_left(t_tile *b) {
-	for (int y=0; y < BOARD_SIZE; y++) {
-		for (int x=0; x < BOARD_SIZE; x++) {
-			if (b[y][x].value == 0) {
-				for (int i=x; i < BOARD_SIZE; i++) {
-					if (i < BOARD_SIZE - 1)
-						b[y][i].value = b[y][i+1].value;
-					else
-						b[y][i] = 0;
-				}
+static void	move_left(t_board *board)
+{
+	unsigned int	value;
+	unsigned int	i;
+
+	for (int y=0; y < BOARD_SIZE; y++)
+   	{
+		for (int x=0; x < BOARD_SIZE; )
+		{
+			value = board->tiles[y][x].value;
+			if (value == 0)
+			{
+				for (i = x + 1; i < BOARD_SIZE && board->tiles[y][i].value == 0; i++)
+					;
+				for (int k = 0; i + k < BOARD_SIZE; k++)
+					board->tiles[y][x + k].value = board->tiles[y][i + k].value;
+				if (i == BOARD_SIZE)
+					x = BOARD_SIZE;
+				continue;
 			}
-			else if (x < BOARD_SIZE - 1 && b[y][x].value == b[y][x+1].value) {
-				b[y][x].value *= 2;
-				for (int i=x+1; i < BOARD_SIZE; i++) {
-					if (i < BOARD_SIZE - 1)
-						b[y][i].value = b[y][i+1].value;
-					else
-						b[y][i] = 0;
-				}
+			for (i = x + 1; i < BOARD_SIZE; i++)
+			{
+				if (board->tiles[y][i].value == value)
+					break;
+				if (board->tiles[y][i].value != 0)
+					i = BOARD_SIZE;
 			}
+			if (i < BOARD_SIZE)
+			{
+				board->tiles[y][x].value *= 2;
+				board->tiles[y][i].value = 0;
+				board->free_tiles++;
+			}
+			x++;
 		}
 	}
 }
 
-static	move_right(t_tile *b) {
-	for (int y=0; y < BOARD_SIZE; y++) {
-		for (int x = BOARD_SIZE-1; 0 <= x; x--) {
-			if (b[y][x].value == 0) {
-				for (int i=x; 0 <= i; i--) {
-					if (0 < i)
-						b[y][i].value = b[y][i-1].value;
-					else
-						b[y][i] = 0;
-				}
+static void	move_right(t_board *board)
+{
+	unsigned int	value;
+	unsigned int	i;
+
+	for (int y=0; y < BOARD_SIZE; y++)
+   	{
+		for (int x = BOARD_SIZE - 1; 0 < x; )
+		{
+			value = board->tiles[y][x].value;
+			if (value == 0)
+			{
+				for (i = x - 1; 0 <= i && board->tiles[y][i].value == 0; i--)
+					;
+				for (int k = 0; 0 <= i - k; k++)
+					board->tiles[y][x - k].value = board->tiles[y][i - k].value;
+				if (i < 0)
+					x = -1;
+				continue;
 			}
-			else if (0 < x && b[y][x].value == b[y][x-1].value) {
-				b[y][x].value *= 2;
-				for (int i = x-1; 0 < i; i--) {
-					if (0 < i)
-						b[y][i].value = b[y][i-1].value;
-					else
-						b[y][i] = 0;
-				}
+			for (i = x - 1; 0 <= i; i--)
+			{
+				if (board->tiles[y][i].value == value)
+					break;
+				if (board->tiles[y][i].value != 0)
+					i = -1;
 			}
+			if (0 <= i)
+			{
+				board->tiles[y][x].value *= 2;
+				board->tiles[y][i].value = 0;
+				board->free_tiles++;
+			}
+			x--;
 		}
 	}
 }
 
-static	move_down(t_tile *b) {
-	for (int x=0; y < BOARD_SIZE; y++) {
-		for (int y=0; x < BOARD_SIZE; x++) {
-			if (b[y][x].value == 0) {
-				for (int i=x; i < BOARD_SIZE; i++) {
-					if (i < BOARD_SIZE - 1)
-						b[y][i].value = b[y][i+1].value;
-					else
-						b[y][i] = 0;
-				}
+static void	move_up(t_board *board)
+{
+	unsigned int	value;
+	unsigned int	i;
+
+	for (int x=0; x < BOARD_SIZE; x++)
+   	{
+		for (int y=0; y < BOARD_SIZE; )
+		{
+			value = board->tiles[y][x].value;
+			if (value == 0)
+			{
+				for (i = x + 1; i < BOARD_SIZE && board->tiles[y][i].value == 0; i++)
+					;
+				for (int k = 0; i + k < BOARD_SIZE; k++)
+					board->tiles[y][x + k].value = board->tiles[y][i + k].value;
+				if (i == BOARD_SIZE)
+					x = BOARD_SIZE;
+				continue;
 			}
-			else if (x < BOARD_SIZE - 1 && b[y][x].value == b[y][x+1].value) {
-				b[y][x].value *= 2;
-				for (int i=x+1; i < BOARD_SIZE; i++) {
-					if (i < BOARD_SIZE - 1)
-						b[y][i].value = b[y][i+1].value;
-					else
-						b[y][i] = 0;
-				}
+			for (i = x + 1; i < BOARD_SIZE; i++)
+			{
+				if (board->tiles[y][i].value == value)
+					break;
+				if (board->tiles[y][i].value != 0)
+					i = BOARD_SIZE;
 			}
+			if (i < BOARD_SIZE)
+			{
+				board->tiles[y][x].value *= 2;
+				board->tiles[y][i].value = 0;
+				board->free_tiles++;
+			}
+			y++;
 		}
 	}
 }
 
-static	move_down(t_tile *b) {
-	for (int x=0; y < BOARD_SIZE; y++) {
-		for (int y = BOARD_SIZE-1; 0 <= x; x--) {
-			if (b[y][x].value == 0) {
-				for (int i=x; 0 <= i; i--) {
-					if (0 < i)
-						b[y][i].value = b[y][i-1].value;
-					else
-						b[y][i] = 0;
-				}
+static void	move_down(t_board *board)
+{
+	unsigned int	value;
+	unsigned int	i;
+	
+	for (int x=0; x < BOARD_SIZE; x++)
+   	{
+		for (int y = BOARD_SIZE - 1; 0 < y; )
+		{
+			value = board->tiles[y][x].value;
+			if (value == 0)
+			{
+				for (i = x - 1; 0 <= i && board->tiles[y][i].value == 0; i--)
+					;
+				for (int k = 0; 0 <= i - k; k++)
+					board->tiles[y][x - k].value = board->tiles[y][i - k].value;
+				if (i < 0)
+					x = -1;
+				continue;
 			}
-			else if (0 < x && b[y][x].value == b[y][x-1].value) {
-				b[y][x].value *= 2;
-				for (int i = x-1; 0 < i; i--) {
-					if (0 < i)
-						b[y][i].value = b[y][i-1].value;
-					else
-						b[y][i] = 0;
-				}
+			for (i = x - 1; 0 <= i; i--)
+			{
+				if (board->tiles[y][i].value == value)
+					break;
+				if (board->tiles[y][i].value != 0)
+					i = -1;
 			}
+			if (0 <= i)
+			{
+				board->tiles[y][x].value *= 2;
+				board->tiles[y][i].value = 0;
+				board->free_tiles++;
+			}
+			y--;
 		}
 	}
 }
-void	move_and_merge(t_board *board, char *input) {
-	t_tile	*b;
 
-	b = board->tiles;
+void	move_and_merge(t_board *board, char *input)
+{
 	if (ft_strcmp(input, "left") == 0)
-		move_left(b);
+		move_left(board);
 	if (ft_strcmp(input, "right") == 0)
-		move_right(b);
+		move_right(board);
 	if (ft_strcmp(input, "up") == 0)
-		move_up(b);
+		move_up(board);
 	if (ft_strcmp(input, "down") == 0)
-		move_down(b);
+		move_down(board);
 }
