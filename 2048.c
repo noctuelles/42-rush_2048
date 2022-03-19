65d1166f717a0f0f2f1adfeb1eafa95dfa95ab8c
noctuelles <plouvel@student.42.fr>
2022-03-19 15:59:06 +0100

    printing numbers in cells ok

--- 2048.c
+++ 2048.c
@@ -6,104 +6,57 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 11:16:16 by plouvel           #+#    #+#             */
-/*   Updated: 2022/03/19 13:36:34 by plouvel          ###   ########.fr       */
+/*   Updated: 2022/03/19 15:57:44 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "2048.h"
 #include <ncurses.h>
 
-void	init_game(t_board *board)
+bool	can_run_game(t_board *board)
 {
-	board->wnd = newwin(board->wnd_nlines,
-			board->wnd_nrows,
-			0,
-			0);
-	for (int y = 0; y < BOARD_SIZE; y++)
+	getmaxyx(stdscr, board->term_nlines, board->term_nrows);
+	if (board->term_nlines < TERM_LINES_MIN || board->term_nrows < TERM_ROWS_MIN)
 	{
-		for (int x = 0; x < BOARD_SIZE; x++)
-		{
-			board->tiles[y][x].wnd = subwin(board->wnd,
-					board->tile_nlines,
-					board->tile_nrows,
-					3 + y * (board->tile_nlines),
-					4 + x * (board->tile_nrows));
-		}
-	}
-}
-
-void	draw_game(t_board *board)
-{
-	refresh();
-	box(board->wnd, ACS_VLINE, ACS_HLINE);
-	wrefresh(board->wnd);
-	for (int y = 0; y < BOARD_SIZE; y++)
-	{
-		for (int x = 0; x < BOARD_SIZE; x++)
-		{
-			box(board->tiles[y][x].wnd, ACS_VLINE, ACS_HLINE);
-			touchwin(board->wnd);
-			wrefresh(board->tiles[y][x].wnd);
-		}
-	}
-}
-
-void	delete_game(t_board *board)
-{
-	for (int y = 0; y < BOARD_SIZE; y++)
-	{
-		for (int x = 0; x < BOARD_SIZE; x++)
-			delwin(board->tiles[y][x].wnd);
-	}
-	delwin(board->wnd);
-}
-
-bool	can_run_game(void)
-{
-	size_t	term_nlines;
-	size_t	term_nrows;
-
-	getmaxyx(stdscr, term_nlines, term_nrows);
-	if (term_nlines < 26 || term_nrows < 71)
-	{
-		mvwaddstr(stdscr, 0, 0, "Your terminal must be at least 71x26 !");
-		mvwaddstr(stdscr, 1, 0, "Press any key to exit...");
+		mvwaddstr(stdscr, 0, 0, STR_ATLEAST_SIZE);
+		mvwaddstr(stdscr, 1, 0, STR_PRESS_KEY);
+		getch();
 		return (false);
 	}
 	return (true);
 }
 
-int	main(void)
+void	init_ncurses(void)
 {
-	t_board	board;
-	int	ch;
-
-	board.tile_nlines = 5;
-	board.tile_nrows = 10;
-	board.wnd_nlines = board.tile_nlines * 5 + 1;
-	board.wnd_nrows = board.tile_nrows * 5;
-
 	initscr();
 	noecho();
 	raw();
+	nonl();
 	cbreak();
 	keypad(stdscr, true);
-	if (can_run_game() == true)
-	{
-		init_game(&board);
-	}
-	while (1)
-	{
-		draw_game(&board);
-		ch = getch();
-		if (ch == KEY_RESIZE)
-		{
-			size_t	term_nlines;
-			size_t	term_nrows;
+}
 
-			getmaxyx(stdscr, board.term_nlines, board.term_nrows);
+int	main(void)
+{
+	t_board	board = {0};
+	int	ch;
+
+	init_ncurses();
+	if (can_run_game(&board) == true)
+	{
+		init_board_wnd(&board);
+		while (1)
+		{
+			draw_board(&board);
+			ch = getch();
+			if (ch == KEY_RESIZE)
+				getmaxyx(stdscr, board.term_nlines, board.term_nrows);
+			else if (ch == 27)
+			{
+				break ;
+			}
 		}
 	}
-	delete_game(&board);
+	delete_board_wnd(&board);
 	endwin();
 }
--- 2048.h
+++ 2048.h
@@ -6,7 +6,7 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 11:16:53 by plouvel           #+#    #+#             */
-/*   Updated: 2022/03/19 12:03:20 by plouvel          ###   ########.fr       */
+/*   Updated: 2022/03/19 15:40:29 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@
 # include <ncurses.h>
 
 # define BOARD_SIZE 4
+
+# define TERM_LINES_MIN 31
+# define TERM_ROWS_MIN  84 
+
+# define TILE_LINES  6
+# define TILE_ROWS   12
+# define BOARD_ROWS  TILE_ROWS * (BOARD_SIZE + 1)
+# define BOARD_LINES TILE_LINES * (BOARD_SIZE + 1)
+
+# define STR_ATLEAST_SIZE   "Your terminal must be at least 110x31"
+# define STR_PLEASE_RESIZE  "Please resize your terminal to resume game."
+# define STR_PRESS_KEY      "Press any key to exit..."
+
 
 typedef struct	s_tile
 {
@@ -27,13 +40,13 @@ typedef struct	s_board
 {
 	WINDOW			*wnd;
 	t_tile			tiles[BOARD_SIZE][BOARD_SIZE];
-	size_t			wnd_nlines;
-	size_t			wnd_nrows;
-	size_t			tile_nlines;
-	size_t			tile_nrows;
 	size_t			term_nlines;
 	size_t			term_nrows;
-	bool			re_draw;
 }				t_board;
+
+
+void	init_board_wnd(t_board *board);
+void	delete_board_wnd(t_board *board);
+void	draw_board(t_board *board);
 
 #endif
